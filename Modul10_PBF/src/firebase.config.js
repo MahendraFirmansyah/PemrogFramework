01b42c77a362fc35f74a74e46d086bb47ae3844f
MahendraFirmansyah <56281719+MahendraFirmansyah@users.noreply.github.com>
2021-04-19 00:42:19 +0700

    Add files via upload

---
+++ Modul10_PBF/src/firebase.config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export default {
+    apiKey: "AIzaSyBv6JeR6y3r7bPVMqL177xB-Nkw3Q_QhwE",
+    authDomain: "modul10pbf-38625.firebaseapp.com",
+    projectId: "modul10pbf-38625",
+    storageBucket: "modul10pbf-38625.appspot.com",
+    messagingSenderId: "462951966526",
+    appId: "1:462951966526:web:4ffd3c1f405d448da1ae6f",
+    measurementId: "G-FFT9ZKZVMM"
+  };
