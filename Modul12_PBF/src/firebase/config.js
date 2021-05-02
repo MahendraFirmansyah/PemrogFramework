77765d4374855991c637177d514990c37b4b17f5
MahendraFirmansyah <56281719+MahendraFirmansyah@users.noreply.github.com>
2021-05-02 19:56:09 +0700

    Add files via upload

---
+++ Modul12_PBF/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDvBJUEnqafsnEr7kprlYBEk_RrtZErXLs",
+    authDomain: "modul11-pbf-db970.firebaseapp.com",
+    projectId: "modul11-pbf-db970",
+    storageBucket: "modul11-pbf-db970.appspot.com",
+    messagingSenderId: "463061337029",
+    appId: "1:463061337029:web:3a93f86ef9cf5ae28501ac",
+    measurementId: "G-Z3NFDRQCMC"
+  }
+  
+  export default firebaseConfig;
+  
